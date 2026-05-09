0fa90d5b227a1c7956fdec81ee3268f2ade3091e
Christine Susic <csusic022@insite.4cd.edu>
2026-05-08 17:59:05 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 // COMSC-210 | Lab 36 | Christine Susic
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "StringBinaryTree.h"
 using namespace std;
 
 int main() {
+    ifstream fin("codes.txt"); //open file
+    int count = 0; //counter
+    string codes; //string to read from file
     StringBinaryTree tree;
     
-    tree.insertNode("Hello");
-    tree.displayInOrder();
+    while (getline(fin, codes)) {
+            tree.insertNode(codes);
+            count++;
+    }
+    
+    tree.displayInOrder(); //display contents
+
+    fin.close(); //close file
+    
+    StringBinaryTree tree2;
+    tree2.insertNode("Hello"); //insert node
+    tree2.displayInOrder(); //display contents
 
     return 0;
 }
